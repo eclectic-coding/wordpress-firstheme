145599a5cdf9aa687d2f9f734d5908c916a7371f
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-03-26 12:18:34 -0400

    Add helper functions

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php get_header(); ?>
-
 <?php if ( have_posts() ) { ?>
 	<?php while ( have_posts() ) { ?>
 		<?php the_post(); ?>
@@ -9,19 +8,12 @@
       </a>
     </h2>
     <div>
-      Posted on <a href="<?php echo get_permalink(); ?>">
-        <time datetime="<?php echo get_the_date( 'c' ); ?>"><?php echo get_the_date(); ?></time>
-      </a>
-      By <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
-			<?php echo get_the_author(); ?>
-      </a>
+      <?php _themename_post_meta(); ?>
     </div>
     <div>
           <?php the_excerpt(); ?>
     </div>
-    <a href="<?php echo the_permalink(); ?>"><?php the_title_attribute(); ?>
-      Read More <span class="u-screen-reader-text">About <?php the_title(); ?></span>
-    </a>
+    <?php _themename_readmore_link(); ?>
 	<?php } ?>
     <?php the_posts_pagination(); ?>
 <?php } else { ?>
