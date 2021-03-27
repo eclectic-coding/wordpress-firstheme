c1724202c0ac89dc5b1707472b52a6151ba504ea
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-03-27 11:32:48 -0400

    Add primary sidebars and dynamic styling

--- functions.php
+++ functions.php
@@ -2,19 +2,6 @@
 
 require_once( 'lib/helpers.php' );
 require_once( 'lib/enqueue-assets.php' );
-
-apply_filters("_themename_no_posts_text", 'no_posts_text');
-/**
- * Description of expected behavior
- *
- * @param $text
- *
- * @return string
- *
- * @since 1.0.0
- */
-function no_posts_text($text) {
-	return $text . 'No posts';
-}
+require_once( 'lib/sidebars.php' );
 
 ?>
--- sidebar.php
+++ sidebar.php
@@ -1,1 +1,3 @@
-Sidebar
+<aside role="complementary">
+	<?php dynamic_sidebar( 'primary-sidebar' ); ?>
+</aside>
